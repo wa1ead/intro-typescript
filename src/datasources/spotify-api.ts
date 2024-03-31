3414051fbd66578b0e7e5ae9af0bf1348e70d744
wa1ead <waliddraa2@gmail.com>
2024-03-31 13:50:54 +0000

    codeGen

--- src/datasources/spotify-api.ts
+++ src/datasources/spotify-api.ts
@@ -1,11 +1,12 @@
 import { RESTDataSource } from '@apollo/datasource-rest'
+import { Playlist } from '../types'
 
 export class SpotifyApi extends RESTDataSource {
   baseURL = "https://spotify-demo-api-fe224840a08c.herokuapp.com/v1/";
 
-  async getFeaturedPlaylists() {
+  async getFeaturedPlaylists(): Promise<Playlist[]> {
     const response = await this.get<{
-      playlists: { items: any[] }
+      playlists: { items: Playlist[] }
     }>('browse/featured-playlists')
     return response?.playlists?.items ?? []
   }
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,5 @@
-const resolvers = {
+import { Resolvers } from "./types"
+const resolvers: Resolvers = {
   Query: {
     featuredPlaylists: (_, __, { dataSources }) => {
       return dataSources.spotifyApi.getFeaturedPlaylists();
